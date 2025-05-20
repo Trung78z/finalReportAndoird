be4bf54200606583eaf502f352a791c30a4280b8
trungpyyy <trungpyyy@gmail.com>
2025-05-20 02:03:52 +0000

    create home activity

--- management/app/src/main/java/com/hcmus/management/activity/LoginActivity.java
+++ management/app/src/main/java/com/hcmus/management/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.hcmus.management.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,11 +16,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         TextView btnLogin = findViewById(R.id.registerLink);
-
+        Button signInButton = findViewById(R.id.signInButton);
         btnLogin.setOnClickListener(v -> {
             Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
+        signInButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
     }
--- management/app/src/main/java/com/hcmus/management/activity/MainActivity.java
+++ management/app/src/main/java/com/hcmus/management/activity/MainActivity.java
@@ -1,24 +1,18 @@
 package com.hcmus.management.activity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hcmus.management.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
-        TextView signInLink = findViewById(R.id.signInLink);
-
-        signInLink.setOnClickListener(v -> {
-            finish();
-        });
+        setContentView(R.layout.activity_home);
     }
 }
