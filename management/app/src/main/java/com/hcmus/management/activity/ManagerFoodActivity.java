1871b7a9607cfbc66f165cc6a13a8053c6abfc1c
trung78z <trungpspy@gmail.com>
2025-05-21 23:28:40 +0700

    create manager fastfood

---
+++ management/app/src/main/java/com/hcmus/management/activity/ManagerFoodActivity.java
@@ -0,0 +1,66 @@
+package com.hcmus.management.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hcmus.management.R;
+import com.hcmus.management.adapter.CartAdapter;
+import com.hcmus.management.model.CartItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagerFoodActivity extends BaseActivity {
+    private CartAdapter adapter;
+    private List<CartItem> cartItems;
+    private ImageButton btnBack;
+    private RecyclerView rvCartItems;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        rvCartItems = findViewById(R.id.rvCartItems);
+        rvCartItems.setLayoutManager(new LinearLayoutManager(this));
+        cartItems = new ArrayList<>();
+        adapter = new CartAdapter(cartItems);
+        rvCartItems.setAdapter(adapter);
+        CartItem item = new CartItem("Burger With Meat", 12.23, 1, true, R.drawable.sample_burger);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+        adapter.addItem(item);
+
+
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            finish();
+        });
+        Button btnAddNew = findViewById(R.id.btnAddNew);
+        btnAddNew.setOnClickListener(v -> {
+            Intent intent = new Intent(this, CreateFastFoodActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_manager_fastfood;
+    }
+
+    @Override
+    protected int getBottomNavigationMenuItemId() {
+        return R.id.nav_food;
+    }
+}
