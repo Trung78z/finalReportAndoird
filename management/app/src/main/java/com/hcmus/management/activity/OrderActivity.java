3b8dde9966e32bc04b5cbbefc99a29c44ae282d9
trung78z <trungpspy@gmail.com>
2025-05-21 23:29:17 +0700

    Create order

--- management/app/src/main/java/com/hcmus/management/activity/OrderActivity.java
+++ management/app/src/main/java/com/hcmus/management/activity/OrderActivity.java
@@ -1,16 +1,26 @@
 package com.hcmus.management.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hcmus.management.R;
+import com.hcmus.management.adapter.CartAdapter;
+import com.hcmus.management.model.CartItem;
 
-public class OrderActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderActivity extends BaseActivity {
     private RecyclerView rvCartItems;
+
+    private CartAdapter adapter;
+    private List<CartItem> cartItems;
     private EditText etPromoCode;
     private Button btnApplyPromo, btnOrderNow;
     private TextView tvTotalItems, tvDiscount, tvTotal;
@@ -18,16 +28,37 @@ public class OrderActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order);
 
         rvCartItems = findViewById(R.id.rvCartItems);
         etPromoCode = findViewById(R.id.etPromoCode);
         btnApplyPromo = findViewById(R.id.btnApplyPromo);
-        btnOrderNow = findViewById(R.id.btnOrderNow);
+        btnOrderNow = findViewById(R.id.btnAddNew);
         tvTotalItems = findViewById(R.id.tvTotalItems);
         tvDiscount = findViewById(R.id.tvDiscount);
         tvTotal = findViewById(R.id.tvTotal);
+        rvCartItems.setLayoutManager(new LinearLayoutManager(this));
 
-        // TODO: Set up RecyclerView adapter, promo code logic, and order button
+        cartItems = new ArrayList<>();
+        adapter = new CartAdapter(cartItems);
+        rvCartItems.setAdapter(adapter);
+
+
+        CartItem item = new CartItem("Burger With Meat", 12.23, 1, true, R.drawable.sample_burger);
+        adapter.addItem(item);
+
+        btnOrderNow.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PaymentActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_order;
+    }
+
+    @Override
+    protected int getBottomNavigationMenuItemId() {
+        return R.id.nav_cart;
     }
 }
