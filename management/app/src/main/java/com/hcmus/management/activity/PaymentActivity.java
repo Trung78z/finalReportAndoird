2101b4957ad8742dab0e98bf1eec3ca51636ada0
trung78z <trungpspy@gmail.com>
2025-05-21 23:29:03 +0700

    Create payment

--- management/app/src/main/java/com/hcmus/management/activity/PaymentActivity.java
+++ management/app/src/main/java/com/hcmus/management/activity/PaymentActivity.java
@@ -1,4 +1,25 @@
 package com.hcmus.management.activity;
 
-public class Payment {
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hcmus.management.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    private ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_payment);
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> {
+            finish();
+        });
+    }
 }
